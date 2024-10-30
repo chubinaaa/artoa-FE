c6fa2967163b76fb566f1080c3f6d106e2fbf3c9
rolandiartmeladze <rartmeladze@gmail.com>
2024-10-30 19:50:19 +0400

    fix: updare strusture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 // .src/app/pages.tsx
 
-import Header from "@/components/Header";
-import Content from "@/components/Content";
-import Footer from "@/components/Footer";
+import Header from "@/components/Header/Header";
+import Content from "@/components/Footer/Content/Content";
+import Footer from "@/components/Footer/Footer";
 
 export default function Home() {
   return (
