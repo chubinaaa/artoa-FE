ff630d44b736e31aa1537f3c17de4b530ecf5d7c
Misho Shubitidze <misho@mshub.dev>
2024-11-03 00:01:31 +0400

    add reusable input

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+const Input = React.forwardRef<
+  HTMLInputElement,
+  React.InputHTMLAttributes<HTMLInputElement>
+>(({ className, type, ...props }, ref) => {
+  return (
+    <input
+      type={type}
+      className={cn(
+        "placeholder:text-muted-foreground bg-input-background flex h-14 w-full rounded-lg border border-input px-6 py-4 transition-colors file:border-0 file:bg-transparent file:font-medium file:text-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+        className,
+      )}
+      ref={ref}
+      {...props}
+    />
+  );
+});
+Input.displayName = "Input";
+
+export { Input };
