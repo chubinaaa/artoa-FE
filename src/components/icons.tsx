0981dcb4a473cd200253bf9fad07865e460efc94
Misho Shubitidze <misho@mshub.dev>
2024-11-03 02:43:20 +0400

    add reusable checkbox

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -106,5 +106,23 @@ export const Icons = {
       />
     </svg>
   ),
+  check: (props: SvgIconProps) => (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M2.66666 8L6.66666 12L13.3333 4"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  ),
   //...rest of the icons
 };
