80ccb30e1c666a61edfa7b2941b16e132f7a9bcd
Misho Shubitidze <misho@mshub.dev>
2024-11-05 02:28:20 +0400

    remove demo

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,12 +1,10 @@
 import { SignUpDialog } from "@/components/auth/sign-up/sign-up-dialog";
-import { WallArtistInAnyDirectionDemo } from "./wall-artists-in-any-direction.demo";
 
 export default function IndexPage() {
   return (
     <div className="size-full">
       <h1>Hello ArtOa!</h1>
       <SignUpDialog />
-      <WallArtistInAnyDirectionDemo />
     </div>
   );
 }
