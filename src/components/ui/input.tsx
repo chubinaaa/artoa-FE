b19f9956043e96488c0c1fba79e4fcb7f7309d41
Misho Shubitidze <misho@mshub.dev>
2024-11-04 12:14:27 +0400

    format

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -10,7 +10,7 @@ const Input = React.forwardRef<
     <input
       type={type}
       className={cn(
-        "placeholder:text-muted-foreground bg-input-background flex h-14 w-full rounded-lg border border-input px-6 py-4 transition-colors file:border-0 file:bg-transparent file:font-medium file:text-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+        "flex h-14 w-full rounded-lg border border-input bg-input-background px-6 py-4 transition-colors file:border-0 file:bg-transparent file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
         className,
       )}
       ref={ref}
