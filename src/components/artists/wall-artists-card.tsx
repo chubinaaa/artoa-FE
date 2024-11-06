42607268454fd2d1f1e580233b1328c1dc3a5ca2
Misho Shubitidze <misho@mshub.dev>
2024-11-06 12:22:41 +0400

    create wall artists in any direction section, work in progress

--- src/components/artists/wall-artists-card.tsx
+++ src/components/artists/wall-artists-card.tsx
@@ -1,78 +1,56 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 
 import Image from "next/image";
 import { Icons } from "../icons";
-import Link from "next/link";
 import { Button } from "../ui/button";
 
-type WallArtistCardProps = {
-  id: number;
+interface Artist {
   name: string;
   handle: string;
-  imageUrl: string;
-  artStyles: string[];
-  description: string;
   rating: number;
-  href: string;
-};
+  tags: string[];
+  image: string;
+}
 
-export function WallArtistCard({
-  name,
-  handle,
-  imageUrl,
-  artStyles,
-  description,
-  href,
-  rating,
-}: WallArtistCardProps) {
+export function WallArtistCard({ artist }: { artist: Artist }) {
   return (
-    <Card className="flex w-80 flex-col gap-2 bg-background">
-      <CardHeader className="p-0">
-        <Image src={imageUrl} alt={name} width={320} height={320} />
-      </CardHeader>
+    <Card className="bg-background">
       <CardContent className="p-0">
-        <div className="flex flex-col gap-2">
-          <div className="flex flex-col gap-1">
-            <div className="flex items-center justify-between">
-              <ul className="flex items-center gap-2">
-                {artStyles.map((style) => (
-                  <li
-                    key={style}
-                    className="text-sm font-semibold text-primary"
-                  >
-                    {style}
-                  </li>
-                ))}
-              </ul>
-              <div className="flex items-center gap-1 text-sm">
-                <Icons.star className="text-primary" /> {rating}
-              </div>
+        <div className="relative aspect-square overflow-hidden">
+          <Image
+            src={artist.image}
+            alt={artist.name}
+            className="size-full object-cover"
+            width={310}
+            height={310}
+          />
+        </div>
+        <div>
+          <div className="mb-2 flex items-center gap-2">
+            {artist.tags.map((tag, i) => (
+              <span
+                key={i}
+                className="hidden text-primary first:block md:block"
+              >
+                {tag}
+              </span>
+            ))}
+            <div className="ml-auto flex items-center gap-1">
+              <Icons.star className="text-accent" />
+              <span>{artist.rating}</span>
             </div>
-            <CardTitle>
-              {name}{" "}
-              <Link href={href}>
-                <span className="text-sm font-normal">{handle}</span>
-              </Link>
-            </CardTitle>
           </div>
-          <CardDescription className="text-foreground">
-            {description}
-          </CardDescription>
+          <h3 className="font-semibold">
+            {artist.name} <span className="font-normal">{artist.handle}</span>
+          </h3>
+          <p className="mt-1 hidden md:block">
+            lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem,
+            voluptate.
+          </p>
         </div>
       </CardContent>
-      <CardFooter className="p-0">
-        <Button asChild>
-          <Link href={href} className="w-full">
-            See More
-          </Link>
-        </Button>
+      <CardFooter className="mt-2 p-0 md:mt-4">
+        <Button className="h-10 w-full md:h-12">See More</Button>
       </CardFooter>
     </Card>
   );
