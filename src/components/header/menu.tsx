0b38319930a58c95a0adaab8a37e571191e8d524
rolandiartmeladze <rartmeladze@gmail.com>
2024-11-08 15:44:54 +0400

    Add responsive menu and menu button in header

--- src/components/header/menu.tsx
+++ src/components/header/menu.tsx
@@ -1,27 +1,18 @@
 import Link from "next/link";
-import { Icons } from "../icons";
 
-export function MenuButton() {
-  return (
-    <div className="md: hidden cursor-pointer max-sm:flex">
-      <Icons.menuButton />
-    </div>
-  );
-}
+const menuItems = [
+  { label: "Home", url: "/" },
+  { label: "Artists", url: "/artists" },
+  { label: "Services", url: "/services" },
+  { label: "About", url: "/about" },
+  { label: "Contact Us", url: "/contact" },
+];
 
 export function Menu() {
-  const MenuItems = [
-    { label: "Home", url: "/" },
-    { label: "Artists", url: "/artists" },
-    { label: "Services", url: "/services" },
-    { label: "About", url: "/about" },
-    { label: "Contact Us", url: "/contact" },
-  ];
-
   return (
     <nav className="md: flex max-sm:hidden">
       <ul className="flex items-center justify-between gap-12">
-        {MenuItems.map((item) => (
+        {menuItems.map((item) => (
           <li key={item.label}>
             <Link href={item.url} className="hover:text-primary">
               {item.label}
--- src/components/header/public-header.tsx
+++ src/components/header/public-header.tsx
@@ -1,15 +1,16 @@
 import { Icons } from "../icons";
-
-import { Menu, MenuButton } from "./menu";
-import { LanguageSwitcher } from "./LanguageSwitcher";
+import { MenuButton } from "./menu-button";
+import { Menu } from "./menu";
 import { HeaderButtons } from "./buttons";
+import { LanguageSwitcher } from "./languageSwitcher";
+
 export function PublicHeader() {
   return (
     <header className="flex w-full justify-between border-b border-border px-6 py-4 md:px-16 md:py-5">
       <div className="flex w-full justify-between">
-        <div className="flex cursor-pointer items-center gap-28 max-md:gap-6 max-sm:gap-3">
+        <div className="flex items-center gap-28 max-md:gap-6 max-sm:gap-3">
           <MenuButton />
-          <Icons.logo className="size-12 max-sm:size-8" />
+          <Icons.logo className="size-12 cursor-pointer max-sm:size-8" />
           <Menu />
         </div>
         <div className="max-ms: flex items-center gap-2 md:gap-8">
