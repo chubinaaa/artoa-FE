e510670fe1b433296789b1d12905eb039eca5d27
Misho Shubitidze <misho@mshub.dev>
2024-11-09 04:02:37 +0400

    create profile-header and extend tailwind to include header height h-22(88px)

--- src/components/header/profile-header.tsx
+++ src/components/header/profile-header.tsx
@@ -1,4 +1,21 @@
+import { Icons } from "../icons";
+import Image from "next/image";
+import { LanguageSwitcher } from "./language-switcher";
+
 export function ProfileHeader() {
-  // TODO: replace me with an actual header
-  return <header>profile header</header>;
+  return (
+    <header className="container mx-auto flex h-22 items-center justify-between">
+      <Icons.logo />
+      <div className="flex items-center gap-3">
+        <Image
+          src="/placeholder.svg"
+          alt="placeholder"
+          width={40}
+          height={40}
+          className="size-5 rounded-full"
+        />
+        <LanguageSwitcher />
+      </div>
+    </header>
+  );
 }
