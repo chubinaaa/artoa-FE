21919a453277ca715f250207989dff09ae8d4975
Misho Shubitidze <misho@mshub.dev>
2024-11-10 17:35:18 +0400

    abstract sign in/sign up form components

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,12 +1,10 @@
-import { Button, buttonVariants } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { Icons } from "@/components/icons";
-import { Input } from "@/components/ui/input";
-import { Checkbox } from "@/components/ui/checkbox";
 import { AuthShell } from "@/components/auth/auth-shell";
-import { Label } from "@/components/ui/label";
 import { SocialButton } from "@/components/auth/sign-in/social-button";
+import { SignInForm } from "./_components/sign-in-form.client";
 
 export default function SignIn() {
   return (
@@ -27,26 +25,7 @@ export default function SignIn() {
             </p>
           </div>
         </div>
-        <form className="flex flex-col gap-6">
-          <div className="flex flex-col gap-2.5">
-            <Label>Enter Login</Label>
-            <Input placeholder="Email" />
-          </div>
-          <div className="flex flex-col gap-2.5">
-            <Label>Enter Password</Label>
-            <Input placeholder="***************" type="password" />
-            <Link
-              className="-mt-0.5 self-end text-xs text-muted-foreground"
-              href="#"
-            >
-              Forget Password
-            </Link>
-          </div>
-          <div className="flex items-center space-x-2 text-xs md:mt-6 md:text-base">
-            <Checkbox /> <Label>Remember Me</Label>
-          </div>
-          <Button size="lg">Log In</Button>
-        </form>
+        <SignInForm />
         <div className="flex items-center gap-3 text-xs">
           <Icons.horizontalLine /> Or <Icons.horizontalLine />
         </div>
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,12 +1,10 @@
-import { Button, buttonVariants } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { Icons } from "@/components/icons";
-import { Input } from "@/components/ui/input";
-import { Checkbox } from "@/components/ui/checkbox";
 import { AuthShell } from "@/components/auth/auth-shell";
-import { Label } from "@/components/ui/label";
 import { SocialButton } from "@/components/auth/sign-in/social-button";
+import { SignUpForm } from "./_components/sign-up-form.client";
 
 export default function SignUp() {
   return (
@@ -27,39 +25,7 @@ export default function SignUp() {
             </p>
           </div>
         </div>
-        <form className="flex flex-col gap-6">
-          <div className="flex flex-col gap-2.5">
-            <Label>Email Address</Label>
-            <Input type="email" placeholder="Email Address" />
-          </div>
-          <div className="flex flex-col gap-2.5">
-            <Label>Enter Password</Label>
-            <Input type="password" placeholder="***************" />
-            <span className="-mt-0.5 self-end text-2xs text-muted-foreground md:text-xs">
-              Use 8 or more characters with mix of letters, numbers & symbols
-            </span>
-          </div>
-          <div className="flex flex-col gap-2.5">
-            <Label>Repeat Password</Label>
-            <Input type="password" placeholder="***************" />
-            <span className="-mt-0.5 self-end text-2xs text-muted-foreground md:text-xs">
-              Use 8 or more characters with mix of letters, numbers & symbols
-            </span>
-          </div>
-          <div className="mt-6 flex items-center space-x-2 text-xs md:text-base">
-            <Checkbox />{" "}
-            <Label>
-              By creating an account, I agree to the{" "}
-              <Link
-                href="/terms"
-                className={cn(buttonVariants({ variant: "link" }), "h-fit p-0")}
-              >
-                Terms and Conditions
-              </Link>
-            </Label>
-          </div>
-          <Button size="lg">Create Account</Button>
-        </form>
+        <SignUpForm />
         <div className="flex items-center gap-3 text-xs">
           <Icons.horizontalLine /> Or <Icons.horizontalLine />
         </div>
