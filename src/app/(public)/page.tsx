f7186687f2e28afb242e967f46f85ea5624dc974
Misho Shubitidze <misho@mshub.dev>
2024-11-13 00:05:43 +0400

    fix import path

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,20 +1,24 @@
-import { Artist } from "@/components/artists/featured-artist-card";
 import { AiGeneratedArtsSection } from "@/components/homepage/ai-generated-arts-section";
 import { ArtoaTeamSection } from "@/components/homepage/artoa-team-section";
 import { FeaturedArtistsSection } from "@/components/homepage/featured-artists-section";
 import { HeroSection } from "@/components/homepage/hero-section";
 import { ServiceSection } from "@/components/homepage/service-section";
 import { WallArtistsSection } from "@/components/homepage/wall-artists-section";
+import { Artist } from "@/types/artist";
 
-const mockArtists: Artist[] = Array.from({ length: 10 }).map((_, index) => ({
-  id: index,
-  name: `Mock Artist ${index}`,
-  handle: `mock-artist-${index}`,
-  image: "/placeholder.svg",
-  tags: ["3D Art", "Digital Art", "Illustration"],
-  rating: Math.floor(Math.random() * 5) + 1,
-  href: `/artists/${index}`,
-}));
+const mockArtists: Array<Artist> = Array.from({ length: 10 }).map(
+  (_, index) => ({
+    id: index,
+    name: `Mock Artist ${index}`,
+    handle: `mock-artist-${index}`,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta neque omnis ea sed, possimus beatae! Dolor quis eligendi doloremque sunt.",
+    image: "/placeholder.svg",
+    tags: ["3D Art", "Digital Art", "Illustration"],
+    rating: Math.floor(Math.random() * 5) + 1,
+    href: `/artists/${index}`,
+  }),
+);
 
 export default function IndexPage() {
   return (
