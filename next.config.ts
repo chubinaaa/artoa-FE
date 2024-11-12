85bfd2fe895b5db107022ff7b6c8c8f2ea4d30d6
Misho Shubitidze <misho@mshub.dev>
2024-11-12 20:13:59 +0400

    revert compiler: removeConsole

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,4 @@ import { NextConfig } from "next";
 export default {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  compiler: {
-    removeConsole: true,
-  },
 } satisfies NextConfig;
