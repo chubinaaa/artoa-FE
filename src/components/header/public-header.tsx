81b80e48d621786502b52e6344f313524713ca21
rolandiartmeladze <rartmeladze@gmail.com>
2024-11-17 00:28:40 +0400

    Fix: Improved header layout and responsiveness / Updated gap spacing and avatar icon visibility

--- src/components/header/public-header.tsx
+++ src/components/header/public-header.tsx
@@ -9,15 +9,15 @@ export function PublicHeader() {
   return (
     <header className="flex w-full justify-between border-b border-border px-6 py-4 md:px-16 md:py-5">
       <div className="flex w-full justify-between">
-        <div className="flex items-center gap-28 max-sm:gap-3">
+        <div className="flex items-center gap-6 md:gap-16 lg:gap-28">
           <MenuButton />
           <Link href={"/"}>
             <Icons.logo className="size-12 cursor-pointer max-sm:size-8" />
           </Link>
           <Navigation />
         </div>
-        <div className="max-ms: flex items-center gap-2 md:gap-8">
-          <Icons.avatarIcon className="block sm:hidden" />
+        <div className="max-ms: flex items-center gap-3 sm:gap-4 md:gap-6">
+          <Icons.avatarIcon className="block md:block lg:hidden" />
           <HeaderButtons />
           <LanguageSwitcher />
         </div>
