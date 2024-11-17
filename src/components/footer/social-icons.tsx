cd671854231144b570a5fb441104c7f5cf193e03
rolandiartmeladze <rartmeladze@gmail.com>
2024-11-17 23:56:35 +0400

    feat: enhance social media icons with aria-labels for accessibility - Added descriptive aria-label attributes to each Link component. - Ensures accessibility by providing meaningful labels for assistive technologies.

--- src/components/footer/social-icons.tsx
+++ src/components/footer/social-icons.tsx
@@ -1,12 +1,33 @@
+import Link from "next/link";
 import { Icons } from "../icons";
 
 export function SocialIcons() {
   return (
     <div className="flex gap-8">
-      <Icons.facebook />
-      <Icons.instagram />
-      <Icons.youtube />
-      <Icons.pinterest />
+      <Link
+        href="https://www.facebook.com/artoa.io/"
+        aria-label="Visit our Facebook page"
+      >
+        <Icons.facebook />
+      </Link>
+      <Link
+        href="https://www.instagram.com/"
+        aria-label="Visit our Instagram page"
+      >
+        <Icons.instagram />
+      </Link>
+      <Link
+        href="https://www.youtube.com/"
+        aria-label="Visit our YouTube channel"
+      >
+        <Icons.youtube />
+      </Link>
+      <Link
+        href="https://www.pinterest.com/"
+        aria-label="Visit our Pinterest page"
+      >
+        <Icons.pinterest />
+      </Link>
     </div>
   );
 }
