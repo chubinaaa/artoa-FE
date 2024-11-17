fd7b6c1bd3825ce782c70b8844efd393c590a9c1
rolandiartmeladze <rartmeladze@gmail.com>
2024-11-18 00:05:34 +0400

    feat: improve accessibility and styling in Subscribe component
    - Updated container to flex layout for better alignment and responsiveness.
    - Changed wrapper from div to form for semantic correctness.
    - Added aria-label attributes to input and button elements for accessibility.
    - Adjusted input and button styling for better layout and visual consistency.
    
    This update enhances accessibility, layout, and user experience in the Subscribe component.

--- src/components/footer/subscribe.tsx
+++ src/components/footer/subscribe.tsx
@@ -2,21 +2,26 @@ import { Icons } from "../icons";
 
 export function Subscribe() {
   return (
-    <div className="max-w-80 gap-8">
-      <p className="text-foreground">
+    <div className="flex flex-col items-start gap-8 bg-inherit">
+      <p className="text-left text-foreground">
         Be the first to hear about new arrivals, promotions, style inspiration,
         and exclusive sneak peeks.
       </p>
-      <div className="flex items-center justify-between rounded-lg border border-solid px-6 py-4">
+      <form className="flex w-full max-w-md items-center gap-4 rounded-lg border border-solid px-6 py-4">
         <input
           type="email"
           placeholder="Your email"
-          className="bg-inherit focus:outline-none"
+          className="grow bg-inherit outline-none"
+          aria-label="Email Address"
         />
-        <button className="flex items-center gap-3 rounded-xl bg-secondary-foreground p-1">
-          <Icons.arrowRight />
+        <button
+          type="submit"
+          className="flex size-6 items-center gap-2.5 rounded-xl bg-foreground p-2"
+          aria-label="Subscribe"
+        >
+          <Icons.arrowRight className="text-secondary-foreground" />
         </button>
-      </div>
+      </form>
     </div>
   );
 }
