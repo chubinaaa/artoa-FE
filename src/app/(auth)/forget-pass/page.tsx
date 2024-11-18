6f59e66d2354d8adfb164e39e31fdbe64139fa73
Giorgi Barishvili <90037916+giorgibarishvili@users.noreply.github.com>
2024-11-18 19:44:39 +0400

    complete forget password enter-new-password step component

--- src/app/(auth)/forget-pass/page.tsx
+++ src/app/(auth)/forget-pass/page.tsx
@@ -1,12 +1,14 @@
 import { AuthShell } from "@/components/auth/auth-shell";
-import WriteEmail from "@/components/auth/forget-pass/write-email";
+import NewPassword from "@/components/auth/forget-pass/new-password";
+// import WriteEmail from "@/components/auth/forget-pass/write-email";
 // import CheckEmail from "@/components/auth/forget-pass/check-email";
 
 export default function ForgotPassword() {
   return (
     <AuthShell>
-      <WriteEmail />
+      {/* <WriteEmail /> */}
       {/* <CheckEmail /> */}
+      <NewPassword />
     </AuthShell>
   );
 }
