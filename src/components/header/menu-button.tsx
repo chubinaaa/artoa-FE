944f1b6903c70ac407e9dd48e8a03702ce385b20
rolandiartmeladze <rartmeladze@gmail.com>
2024-11-19 00:25:57 +0400

    Remove src/components/header/menu-button.tsx from fix/footer-bug-ui-improvements

--- src/components/header/menu-button.tsx
+++
@@ -1,9 +0,0 @@
-import { Icons } from "../icons";
-
-export function MenuButton() {
-  return (
-    <div className="cursor-pointer md:flex lg:hidden">
-      <Icons.menuButton />
-    </div>
-  );
-}
