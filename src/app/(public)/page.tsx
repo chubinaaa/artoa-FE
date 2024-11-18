ce94344b4fc87b5c152fdcc73753c9cceea3b6d1
Misho Shubitidze <misho@mshub.dev>
2024-11-18 16:06:33 +0400

    fix mockArtist data

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -10,9 +10,8 @@ const mockArtists: Array<Artist> = Array.from({ length: 10 }).map(
   (_, index) => ({
     id: index,
     name: `Mock Artist ${index}`,
-    handle: `mock-artist-${index}`,
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta neque omnis ea sed, possimus beatae! Dolor quis eligendi doloremque sunt.",
+    handle: `@artst-${index}`,
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     image: "/placeholder.svg",
     tags: ["3D Art", "Digital Art", "Illustration"],
     rating: Math.floor(Math.random() * 5) + 1,
