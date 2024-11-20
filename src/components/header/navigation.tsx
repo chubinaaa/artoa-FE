8fd474dbf37cf94dedfbe15d4f88671af7ec2750
Misho Shubitidze <misho@mshub.dev>
2024-11-20 20:47:23 +0400

    Discard changes to src/components/header/navigation.tsx

--- src/components/header/navigation.tsx
+++ src/components/header/navigation.tsx
@@ -1,6 +1,4 @@
-"use client";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 
 const menuItems = [
   { label: "Home", url: "/" },
@@ -10,42 +8,17 @@ const menuItems = [
   { label: "Contact Us", url: "/contact-us" },
 ];
 
-function isActiveMenuItem(cuurentPath: string, itemUrl: string): boolean {
-  return cuurentPath === itemUrl;
-}
-
 export function Navigation() {
-  // need add logic when user is authoriser  isStaf = true;
-  const isStaf = false;
-
-  const pathname = usePathname();
-
   return (
     <nav className="hidden lg:flex">
-      <ul className="flex items-center justify-between gap-4 text-sm lg:gap-8">
+      <ul className="flex items-center justify-between gap-12">
         {menuItems.map((item) => (
           <li key={item.label}>
-            <Link
-              aria-label={`Navigate to ${item.label}`}
-              tabIndex={0}
-              href={item.url}
-              className={`hover:text-primary ${
-                isActiveMenuItem(pathname, item.url)
-                  ? "font-bold text-primary"
-                  : ""
-              }`}
-            >
+            <Link href={item.url} className="hover:text-primary">
               {item.label}
             </Link>
           </li>
         ))}
-        {isStaf && (
-          <li>
-            <Link href="/monitoring" className="hover:text-primary">
-              Monitoring (CTA)
-            </Link>
-          </li>
-        )}
       </ul>
     </nav>
   );
