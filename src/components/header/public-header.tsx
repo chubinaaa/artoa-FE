e1295a8adbd4d55d71582dafedea54c45467f249
Misho Shubitidze <misho@mshub.dev>
2024-11-20 20:47:45 +0400

    Discard changes to src/components/header/public-header.tsx

--- src/components/header/public-header.tsx
+++ src/components/header/public-header.tsx
@@ -7,17 +7,17 @@ import Link from "next/link";
 
 export function PublicHeader() {
   return (
-    <header className="flex w-full justify-between border-b py-4 md:py-5">
-      <div className="container mx-auto flex justify-between">
-        <div className="flex items-center gap-6 md:gap-16 lg:gap-28">
+    <header className="flex w-full justify-between border-b border-border px-6 py-4 md:px-16 md:py-5">
+      <div className="flex w-full justify-between">
+        <div className="flex items-center gap-28 max-sm:gap-3">
           <MenuButton />
           <Link href={"/"}>
             <Icons.logo className="size-12 cursor-pointer max-sm:size-8" />
           </Link>
           <Navigation />
         </div>
-        <div className="max-ms: flex items-center gap-3 sm:gap-4 md:gap-6">
-          <Icons.avatarIcon className="block md:block lg:hidden" />
+        <div className="max-ms: flex items-center gap-2 md:gap-8">
+          <Icons.avatarIcon className="block sm:hidden" />
           <HeaderButtons />
           <LanguageSwitcher />
         </div>
