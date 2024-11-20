4ee342c5f27df42600d98ee7219acf430beaf979
Misho Shubitidze <misho@mshub.dev>
2024-11-20 20:47:13 +0400

    Discard changes to src/components/header/language-switcher.tsx

--- src/components/header/language-switcher.tsx
+++ src/components/header/language-switcher.tsx
@@ -2,24 +2,17 @@ import { Icons } from "../icons";
 
 export function LanguageSwitcher() {
   return (
-    <div className="relative flex cursor-pointer items-center gap-2">
+    <div className="flex cursor-pointer items-center gap-2">
       <Icons.globe />
-      <div className="relative">
-        <select
-          aria-label="language-switcher"
-          name="switch-language"
-          id="language-select"
-          className="cursor-pointer appearance-none bg-transparent pl-2 pr-8 text-sm focus:outline-none"
-        >
-          <option aria-label="English" className="text-black" value="en">
-            En
-          </option>
-          <option aria-label="Georgian" className="text-black" value="ge">
-            Ge
-          </option>
-        </select>
-        <Icons.arrowDown className="pointer-events-none absolute right-2 top-1/2 -translate-y-1/2" />
-      </div>
+      <select
+        name="switch-language"
+        id="language-select"
+        className="focus: cursor-pointer appearance-none border-0 bg-inherit outline-none"
+      >
+        <option value="en">En</option>
+        <option value="ge">Ge</option>
+      </select>
+      <Icons.arrowDown className="size-3" />
     </div>
   );
 }
