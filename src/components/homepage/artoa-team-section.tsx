abaa5c474858ae591bc72df7edefb2d7d99db921
rolandiartmeladze <rartmeladze@gmail.com>
2024-11-24 00:18:16 +0400

    feat/ update UI detalie

--- src/components/homepage/artoa-team-section.tsx
+++ src/components/homepage/artoa-team-section.tsx
@@ -7,7 +7,7 @@ export function ArtoaTeamSection() {
   return (
     <section className="container mx-auto">
       <div>
-        <h1 className="text-center font-sans text-7xl font-bold leading-[7px]">
+        <h1 className="mt-6 text-center font-sans text-7xl font-bold">
           Find Your Team Of The Digital Future
         </h1>
         <div className="mt-11 flex justify-between">
