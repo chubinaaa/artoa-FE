0c4c05350c8da0af021aa2bcffc8a031e6c37431
Misho Shubitidze <misho@mshub.dev>
2024-11-26 02:34:22 +0400

    desktop

--- src/components/homepage/artoa-team-section.tsx
+++ src/components/homepage/artoa-team-section.tsx
@@ -1,16 +1,25 @@
-import { TeamConfig } from "@/config/team";
-import { TeamItemCard } from "../team/team-card";
+import Image from "next/image";
+import { teamMembers } from "@/config/team";
 
 export function ArtoaTeamSection() {
   return (
-    <section className="container mx-auto">
-      <div>
-        <h1 className="mt-6 text-center font-sans text-7xl font-bold">
+    <section>
+      <div className="container mx-auto py-28">
+        <h1 className="mt-6 text-center font-sans text-6xl font-bold">
           Find Your Team Of The Digital Future
         </h1>
-        <div className="mt-11 flex justify-between">
-          {TeamConfig.map((employee, index) => (
-            <TeamItemCard key={index} employee={employee} />
+        <div className="grid grid-cols-3 overflow-x-scroll pt-40 md:gap-5">
+          {teamMembers.map((member, index) => (
+            <div className="relative" key={index}>
+              <div className="aspect-square rounded-t-full bg-primary"></div>
+              <Image
+                className="absolute -bottom-px self-center"
+                src={member.imageUrl}
+                width={360}
+                height={560}
+                alt={member.name}
+              />
+            </div>
           ))}
         </div>
       </div>
