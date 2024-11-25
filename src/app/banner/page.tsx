9f13e3b9ddbdeed8773f663206d2393539b8d47c
IzaDoliashvili <142500700+IzaDoliashvili@users.noreply.github.com>
2024-11-25 18:25:40 +0400

    added mobile_responsive

--- src/app/banner/page.tsx
+++ src/app/banner/page.tsx
@@ -3,14 +3,12 @@ import { Button } from "@/components/ui/button";
 export default function Banner() {
   return (
     <section className="relative bg-cover bg-center">
-      <div className="w-full bg-[url('/banner/image/Mask.png')]">
-        <div className="px-8 py-16">
-          <div className="ml-10 flex w-5/6 flex-col">
-            <h1 className="text-4xl font-bold md:text-5xl">
-              Discover The Imaginary World
-            </h1>
-            <div className="mt-4 h-1 w-11/12 bg-primary"></div>
-            <div className="my-6 flex flex-col items-start gap-y-1 text-4xl leading-tight">
+      <div className="bg-[url('/banner/image/Mask_mobile.png')] bg-no-repeat sm:bg-[url('/banner/image/Mask.png')]">
+        <div className="px-8 py-16 sm:px-8 sm:py-16">
+          <div className="ml-0 flex flex-col sm:ml-10 sm:w-5/6">
+            <h1 className="text-4xl font-bold">Discover The Imaginary World</h1>
+            <div className="mt-4 h-1 w-11/12 bg-primary sm:w-11/12"></div>
+            <div className="my-6 flex flex-col items-start text-lg leading-relaxed sm:text-xl sm:leading-tight md:text-2xl">
               <span className="">
                 Instantly transform any space with our-powered art generator.
               </span>
@@ -27,22 +25,22 @@ export default function Banner() {
         </div>
       </div>
       <div className="bg-secondary py-10 text-gray-300">
-        <div className="m-auto flex w-5/6 justify-between">
+        <div className="ml-4 flex w-5/6 flex-row justify-between gap-6 sm:m-auto">
           <div className="text-center">
-            <p className="mb-4 text-6xl font-bold">70+</p>
-            <p className="text-4xl">Wall Artists</p>
+            <p className="mb-4 text-3xl font-bold md:text-6xl">70+</p>
+            <p className="text-lg md:text-4xl">Wall Artists</p>
           </div>
           <div className="text-center">
-            <p className="mb-4 text-6xl font-bold">50+</p>
-            <p className="text-4xl">Customers</p>
+            <p className="mb-4 text-3xl font-bold md:text-6xl">50+</p>
+            <p className="text-lg md:text-4xl">Customers</p>
           </div>
           <div className="text-center">
-            <p className="mb-4 text-6xl font-bold">20+</p>
-            <p className="text-4xl">Completed Projects</p>
+            <p className="mb-4 text-3xl font-bold md:text-6xl">20+</p>
+            <p className="text-lg md:text-4xl">Completed Projects</p>
           </div>
           <div className="text-center">
-            <p className="mb-4 text-6xl font-bold">300+</p>
-            <p className="text-4xl">Art pieces sold</p>
+            <p className="mb-4 text-3xl font-bold md:text-6xl">300+</p>
+            <p className="text-lg md:text-4xl">Art pieces</p>
           </div>
         </div>
       </div>
