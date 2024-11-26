68c300b9ac4e1e2511ab401d1a2a1f8338bc8746
Misho Shubitidze <misho@mshub.dev>
2024-11-26 13:37:11 +0400

    fix bg colors on mobile

--- src/components/homepage/ai-generated-arts-section.tsx
+++ src/components/homepage/ai-generated-arts-section.tsx
@@ -1,5 +1,7 @@
 export function AiGeneratedArtsSection() {
   return (
-    <section className="container mx-auto">ai-generated-arts-section</section>
+    <section className="container mx-auto bg-secondary md:bg-background">
+      ai-generated-arts-section
+    </section>
   );
 }
