b71fd7f4a1ba95f035ae5503bca317c70db3cbcd
rolandiartmeladze <rartmeladze@gmail.com>
2024-12-26 18:21:44 +0400

    Added useState and useEffect hooks to track scroll position

--- src/components/header/public-header.tsx
+++ src/components/header/public-header.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState, useEffect } from "react";
 import { Icons } from "../icons";
 import { MenuButton } from "./menu-button";
 import { Navigation } from "./navigation";
@@ -6,8 +8,31 @@ import { LanguageSwitcher } from "./language-switcher";
 import Link from "next/link";
 
 export function PublicHeader() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <header className="flex w-full justify-between border-b border-border px-6 py-4 lg:px-14 lg:py-5">
+    <header
+      className={`${
+        isScrolled
+          ? "fixed left-0 top-0 z-50 bg-black transition-all duration-500 ease-in-out"
+          : "relative"
+      } flex w-full justify-between border-b border-border px-6 py-4 lg:px-14 lg:py-5`}
+    >
       <div className="flex w-full justify-between">
         <div className="flex items-center md:gap-8 lg:gap-28">
           <Link href={"/"} aria-label="Logo ArtOa">
