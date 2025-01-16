6122a13092ae7d14be27a3f674533f80cd37fd34
Roland Artmeladze <rartmeladze@gmail.com>
2025-01-16 12:06:16 -0800

    feat/update Orders Prover component detalies

--- src/app/(user)/(profile)/profile/components/order-popover.tsx
+++ src/app/(user)/(profile)/profile/components/order-popover.tsx
@@ -12,19 +12,33 @@ interface OrderPopover {
 export default function OrderPopover({ order, open, onClose }: OrderPopover) {
   return (
     <Popover open={open} onOpenChange={onClose}>
-      <PopoverContent className="absolute left-11 flex w-fit flex-col gap-4 rounded-lg bg-background p-4">
-        <header>{order.tags}</header>
-        <p>Do you want to delete this project?</p>
-        <Button
-          variant="secondary"
-          onClick={onClose}
-          className="bg-destructive"
-        >
-          <Link href={`/orders?id=${order.id}&action=delete`}>Delete</Link>
-        </Button>
-        <Button variant="outline" onClick={onClose}>
-          Cancel
-        </Button>
+      <PopoverContent className="absolute top-60 flex w-full flex-col items-center gap-4 rounded-t-xl bg-background p-4">
+        <div className="h-1 w-12 rounded-full bg-foreground"></div>
+        <div className="flex flex-col items-center gap-4">
+          <div className="flex w-full justify-center gap-4 border-b border-secondary-foreground pb-4">
+            <samp className="inline rounded-md border border-secondary-foreground p-2 text-sm font-semibold text-foreground">
+              {order.tags}
+            </samp>
+          </div>
+
+          <p className="text-base">Do you want to delete this project?</p>
+          <Button
+            variant="default"
+            onClick={onClose}
+            className="w-full bg-destructive"
+          >
+            <Link href={`/orders?id=${order.id}&action=delete`}>Delete</Link>
+          </Button>
+          <Button
+            variant="secondary"
+            onClick={onClose}
+            className="w-full bg-secondary-foreground text-card-foreground"
+          >
+            Cancel
+          </Button>
+
+          <div className="h-1 w-full rounded-full bg-foreground"></div>
+        </div>
       </PopoverContent>
     </Popover>
   );
