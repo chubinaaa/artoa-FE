0348649529935ddf389dff0248705056a7baab7a
Misho Shubitidze <misho@mshub.dev>
2025-01-28 00:29:58 +0400

    add mobile sidebar to pages

--- src/app/(user)/(profile)/profile/page.tsx
+++ src/app/(user)/(profile)/profile/page.tsx
@@ -1,9 +1,14 @@
+import { MobileSidebar } from "./_components/mobile-sidebar";
 import PersonalInfo from "./_components/personal-info";
 import UserInfo from "./_components/user-info";
 
 export default function ProfilePage() {
   return (
     <div>
+      <h1 className="inline-flex gap-3 text-xl font-bold text-primary md:hidden md:text-3xl">
+        <MobileSidebar />
+        My Profile
+      </h1>
       <UserInfo />
       <PersonalInfo />
     </div>
