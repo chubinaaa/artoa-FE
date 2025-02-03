52b0a5e1440b8cb5461477bf7af99d78b1394bbb
Roland Artmeladze <rartmeladze@gmail.com>
2025-02-03 08:05:36 -0800

    update component ref additional detalies

--- src/app/(user)/(profile)/portfolio/add-project/page.tsx
+++ src/app/(user)/(profile)/portfolio/add-project/page.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/ui/button";
 import AddProjectAside from "../components/aside";
 import AddProjectContainet from "../components/container";
+import Link from "next/link";
 
 export default function AddProjectPage() {
   return (
@@ -11,6 +13,14 @@ export default function AddProjectPage() {
         <div className="grid w-full grid-cols-[69%_29%] gap-5">
           <AddProjectContainet />
           <AddProjectAside />
+        </div>
+        <div className="float-end flex justify-end gap-12">
+          <Button asChild className="px-9 py-6" variant="secondary">
+            <Link href="./"> Cancel </Link>
+          </Button>
+          <Button className="px-9 py-6" variant="default">
+            Publish
+          </Button>
         </div>
       </div>
     </main>
--- src/app/(user)/(profile)/portfolio/components/container.tsx
+++ src/app/(user)/(profile)/portfolio/components/container.tsx
@@ -1,7 +1,43 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 export default function AddProjectContainet() {
   return (
-    <div className="flex flex-col space-y-6 rounded-2xl border border-muted-foreground bg-secondary p-6">
-      additional
+    <div className="flex flex-row space-x-6 rounded-2xl border border-muted-foreground bg-secondary p-6">
+      <form className="flex w-full flex-col gap-6">
+        <div className="flex flex-col gap-2.5">
+          <Label>Title</Label>
+          <Input placeholder="title" />
+        </div>
+        <div className="flex flex-col gap-2.5">
+          <Label>Description</Label>
+          <Textarea
+            name="description"
+            className="bg-input-background"
+            placeholder="Add order  Brief (Description)"
+            rows={4}
+          />
+        </div>
+        <div className="flex flex-col gap-2.5">
+          <Select>
+            <SelectTrigger>
+              <SelectValue placeholder="Location" />
+            </SelectTrigger>
+            <SelectContent className="max-w-fit">
+              <SelectItem value="Tbilisi">Tbilisi</SelectItem>
+              <SelectItem value="Rustavi">Rustavi</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+      </form>
     </div>
   );
 }
